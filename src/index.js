b0445b7515951bc659176bb016e454bc337b30ec
Lynn Hanananer <66330100+lynnhanananer@users.noreply.github.com>
2024-08-15 23:30:42 -0400

    clean up folder structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const fs = require('node:fs')
 const path = require('node:path')
 const { Client, Collection, GatewayIntentBits } = require('discord.js')
-const { token } = require('./config.json')
+const { token } = require('../config.json')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 client.commands = new Collection()
 client.cooldowns = new Collection()
-const foldersPath = path.join(__dirname, './src/commands')
+const foldersPath = path.join(__dirname, './commands')
 const commandFolders = fs.readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
@@ -28,7 +28,7 @@ for (const folder of commandFolders) {
     }
 }
 
-const eventsPath = path.join(__dirname, './src/events')
+const eventsPath = path.join(__dirname, './events')
 const eventFiles = fs
     .readdirSync(eventsPath)
     .filter((file) => file.endsWith('.js'))
